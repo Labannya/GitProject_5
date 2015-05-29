040380bd857aa17117f4c408cd786772642a9530
Labannya <labannyasaha@gmail.com>
2015-05-29 11:28:06 +0100

    Another change is there

---
+++ src/main/java/CompleteBranching.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nissan on 5/29/15.
+ */
+public class CompleteBranching {
+    public static void main(String[] args) {
+        System.out.println("Git is running well");
+    }
+}
